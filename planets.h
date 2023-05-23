bbffc6c186a3edf25beceb337f57eb21a54ed789
zeppi2 <106450562+zeppi2@users.noreply.github.com>
2023-05-23 16:59:51 -0400

    Create planets.h

---
+++ planets.h
@@ -0,0 +1,13 @@
+#ifndef __PLANETS_H__
+#define __PLANETS_H__
+#define SUN {0.0,0.0,0.0,        0.0,0.0,0.0,      1.989e30 }
+#define MERCURY { 57.909e9,0.0,0.0,   0.0,47.36e3,0.0,  0.33011e24 }
+#define VENUS { 108.209e9,0.0,0.0,  0.0,35.02e3,0.0,  4.8675e24 }
+#define EARTH { 149.596e9,0.0,0.0,  0.0,29.78e3,0.0,  5.9724e24 }
+#define MARS { 227.923e9,0.0,0.0,  0.0,24.07e3,0.0,  0.64171e24 }
+#define JUPITER { 778.570e9,0.0,0.0,  0.0,13e3,0.0,     1898.19e24 }
+#define SATURN { 1433.529e9,0.0,0.0, 0.0,9.68e3,0.0,   568.34e24 }
+#define URANUS { 2872.463e9,0.0,0.0, 0.0,6.80e3,0.0,   86.813e24 }
+#define NEPTUNE { 4495.060e9,0.0,0.0, 0.0,5.43e3,0.0,   102.413e24 }
+//Sorry Pluto, you are not a planet anymore.  Thanks Neil DeGrass Tyson!!!
+#endif
